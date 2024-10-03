3a8c8260d74299cb18010712bc128f79233ad476
ballisticfox <spotthedot2345@gmail.com>
2024-10-03 01:40:49 -0500

    Organize textures, reexport to correct dds compression
    - Textures are now organized by placement in the system
    - Correct texture formatting and mipmapping options are used, saving 300mb~
    - Lower resolution packs are now the same as the 16k, just downscaled to their respective resolution

---
+++ 4096/PluginData/resize.py
@@ -0,0 +1,36 @@
+import os
+from wand.image import Image
+import subprocess
+
+def Resize():
+    maxSize = 2048
+    
+    path = os.path.dirname(os.path.abspath(__file__))
+    folders = os.listdir(path)[:-1]
+    for folder in folders:
+        folderFiles = os.listdir(path+"\\"+folder)
+        files = [folder + "\\" + file for file in folderFiles]
+        # print(files)
+        files = [file for file in files if ".bin" not in file]
+        files = [file for file in files if "Biomes" not in file]
+        files = [file for file in files if "Height" not in file]
+        files = [file[:-4] for file in files]
+        
+        # with open(file = "OversizeList.txt", mode='w') as txt:
+        for file in files:
+            with Image(filename = file+".dds") as img:
+                if img.width > maxSize:
+                    print(file)
+                    # DDS_Type = img.compression
+                    # print("Over maxSize, exporting to png")
+                    # img.resize(maxSize, int(maxSize/2))
+                    # img.save(filename=file+".tif")
+                    # # 15 -> DXT1
+                    # # 18 -> DXT5
+                    # # 19 -> DXT5_nm
+                    # subprocess.run(f"nvtt_export {file}.tif -o {file}2.dds -f 15",shell=False)
+                        
+    
+    
+    
+Resize()
